eed3c4ce6db2124783e3a46821d9a465ee7692c2
tim <tim@ubuntu.ubuntu-domain>
2009-12-27 08:30:00 -0500

    new deployment files part 2

---
+++ config/deploy.rb
@@ -0,0 +1,50 @@
+set :application, "polco"
+load 'deploy' if respond_to?(:namespace) # cap2 differentiator
+
+#set :scm_passphrase, "polco111" #This is your custom users password
+#set :user, "deployer"
+
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+set :branch, "master"
+
+set :git_shallow_clone, 1
+set :git_enable_submodules, 1
+
+set :stages, %w(production staging)
+require 'capistrano/ext/multistage'
+ 
+server "74.207.224.12", :app, :web, :db, :primary => true
+ 
+set :user, 'root'
+set :keep_releases, 3
+set :repository,  "git@github.com:tbbooher/polco.git"
+set :use_sudo, false
+set :scm, "git"
+set :deploy_via, :copy
+ 
+# this will make sure that capistrano checks out the submodules if any
+set :git_enable_submodules, 1
+ 
+set(:application) { "polco_#{stage}" } # replace gitlearn with your application name
+set (:deploy_to) { "/var/www/#{application}" }
+set :copy_remote_dir, "/var/www/tmp"
+ 
+# source: http://tomcopeland.blogs.com/juniordeveloper/2008/05/mod_rails-and-c.html
+namespace :deploy do
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+ 
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app do ; end
+  end
+  
+  desc "invoke the db migration"
+  task:migrate, :roles => :app do
+    send(run_method, "cd #{current_path} && rake db:migrate RAILS_ENV=#{stage} ")
+  end
+  
+end
